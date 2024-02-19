7f5bc1944f6dbd6ad16f751214eefc90ba0efc56
mathildaHERR <mathilda.herrell@gmail.com>
2024-02-18 22:59:22 -0500

    dropdown avec hover! (je laisse click en commentaires si on change d'idée)

--- exoweb/src/components/Dropdown.js
+++ exoweb/src/components/Dropdown.js
@@ -3,12 +3,27 @@ import { Link } from "react-router-dom";
 import './Dropdown.css';
 
 function Dropdown() {
-  const [clicked, setClicked] = useState(false);
+  const [hovered, setHovered] = useState(false);
+  // const [clicked, setClicked] = useState(false);
+
+  // const toggleDropdown = () => {
+  //   setClicked(!clicked);
+  // }
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+  }
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+  }
  
   return (
-    <div className="team-container">
-          <p className="page-link" onClick={()=>setClicked(!clicked)}>Nos équipes</p>
-          {clicked && <div className="dropdown-container">
+    <div className="team-container" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <p className="page-link">Nos équipes</p>
+          {/* <p className="page-link" onClick={toggleDropdown}>Nos équipes</p> */}
+          {hovered && <div className="dropdown-container">
+          {/* {(clicked || hovered) && <div className="dropdown-container"> */}
             <Link to="/">2024</Link>
             <Link to="/">2023</Link>
             <Link to="/">2022</Link>
