1bddca1ee2f3f973476dae34528d14b6f95db2e6
mathildaHERR <mathilda.herrell@gmail.com>
2024-02-22 22:48:35 -0500

    debut de teams (manque du clean up dans team.js et meilleur hover)

---
+++ exoweb/src/components/StudentCard.js
@@ -0,0 +1,56 @@
+import React, {useState} from "react";
+import './StudentCard.css';
+
+function StudentCard(props) {
+  const name = props.name;
+  const title = props.title;
+  const [visible, setVisible] = useState(false);
+  const [height, setHeight] = useState(0);
+  const [rising, setIsRising] = useState(false);
+  const maxHeight = 191;
+
+  function timeout(delay) {
+    return new Promise(res => setTimeout(res, delay));
+  }
+
+  const handleMouseEnter = async () => {
+    if (rising) {
+      return;
+    }
+    setIsRising(true);
+    setVisible(true);
+    for (var i = height; i <= maxHeight ; i++) {
+      setHeight(i);
+      await timeout(2);
+    }
+    setIsRising(false);
+  }
+
+  const handleMouseLeave = async () => {
+    if (rising) {
+      return;
+    }
+    setIsRising(true);
+    for (var i = height; i > 0 ; i--) {
+      setHeight(i);
+      await timeout(2);
+    }
+    setVisible(false);
+    setIsRising(false);
+  }
+
+  return (
+    <div className="student-card">
+      <div className="picture-container" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div className="temporary-picture"></div>
+        {visible && <div className="description-cover" style={{height, top: `calc(100% - ${height}px)`}}>
+          <p className="description-text" style={{height: "75%"}}>so long as you remember who you used to be. i wont forget not one word of this, not ever. i will always remember, when the doctor was me.</p>
+        </div>}
+      </div>
+      <p className="student-name">{name}</p>
+      <p className="student-title">{title}</p>     
+    </div>
+  )
+}
+
+export default StudentCard;
--- exoweb/src/pages/Team.js
+++ exoweb/src/pages/Team.js
@@ -1,14 +1,46 @@
 import React from "react";
 import NavBar from "../components/NavBar"; // Import the NavBar component
 import Footer from "../components/Footer"; // Import the Footer component
+import StudentCard from "../components/StudentCard";
+import "./Team.css"
 
 function Team() {
     return (
-        <div>
-            <NavBar /> {/* Add the NavBar component */}
-            <h2>Our Team</h2>
-            <p>Our team is made up of the best developers in the world. We are dedicated to making the best software for you.</p>
-            <Footer></Footer>
+        <div className="team-page">
+            <NavBar/> {/* Add the NavBar component */}
+            <p className="team-header">Équipe 20xx</p>
+            <div className="member-type-container">
+                <p className="team-subtitle">Exécutif</p>
+                <div className="cards-container">
+                    <StudentCard name="*nom*" title="Directrice générale"></StudentCard>
+                    <StudentCard name="*nom*" title="Directeur technique"></StudentCard>
+                    <StudentCard name="*nom*" title="Directrice des communications"></StudentCard>
+                    <StudentCard name="*nom*" title="Trésorerie"></StudentCard>
+                </div>
+            </div>
+            <div className="member-type-container">
+                <p className="team-subtitle">Chefs d'équipe</p>
+                <div className="cards-container">
+                <StudentCard name="*nom*" title="Hydrogène"></StudentCard>
+                <StudentCard name="*nom*" title="Électronique"></StudentCard>
+                <StudentCard name="*nom*" title="Structure"></StudentCard>
+                </div>
+                <div className="cards-container">
+                    <StudentCard name="*nom*" title="Systèmes"></StudentCard>
+                    <StudentCard name="*nom*" title="Électrique"></StudentCard>
+                    <StudentCard name="*nom*" title="Chargé du développement durable"></StudentCard>
+                </div>
+            </div>
+            <div className="member-type-container">
+               <p className="team-subtitle">Membres</p>
+                <div className="cards-container">
+                    <StudentCard name="*nom*" title="*titre*"></StudentCard>
+                    <StudentCard name="*nom*" title="*titre*"></StudentCard>
+                    <StudentCard name="*nom*" title="*titre*"></StudentCard>
+                    <StudentCard name="*nom*" title="*titre*"></StudentCard>
+                </div> 
+            </div>
+            <Footer/> 
         </div>
     )
 }
