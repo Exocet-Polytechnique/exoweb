3900c347985e5aa80ba5301eace0431a1477689d
damd <charles-francis.damedey@polymtl.ca>
2024-06-27 16:56:11 -0400

    add bios

--- exoweb/src/pages/team.js
+++ exoweb/src/pages/team.js
@@ -26,31 +26,44 @@ function Team() {
             <div className="member-type-container">
                 <p className="team-subtitle">Exécutif</p>
                 <div className="cards-container">
-                    <StudentCard image={Anne} bio="test" name="Anne Raymond" title="Directrice générale"></StudentCard>
-                    <StudentCard image={Emile} bio="test" name="Émile Brousseau" title="Directeur technique"></StudentCard>
-                    <StudentCard image={Tiphaine} bio="test" name="Tiphaine Le Rhun" title="Directrice des communications"></StudentCard>
-                    <StudentCard image={Justin} bio="test" name="Justin Lamouche" title="Trésorerie"></StudentCard>
+                    <StudentCard image={Anne} bio="test" 
+                    name="Anne Raymond" title="Directrice générale"></StudentCard>
+                    <StudentCard image={Emile} bio="test" 
+                    name="Émile Brousseau" title="Directeur technique"></StudentCard>
+                    <StudentCard image={Tiphaine} bio="Je m'occupe de tout ce qui est en lien avec la communication de l'équipe. De plus, pour me rendre utile à l'atelier, je perse, ponce, coupe, troue, vis..."
+                     name="Tiphaine Le Rhun" title="Directrice des communications"></StudentCard>
+                    <StudentCard image={Justin} bio="test" 
+                    name="Justin Lamouche" title="Trésorerie"></StudentCard>
                 </div>
             </div>
             <div className="member-type-container">
                 <p className="team-subtitle">Chefs d'équipe</p>
                 <div className="cards-container">
-                <StudentCard image={Francis} bio="test" name="Francis Bouchaud" title="Hydrogène"></StudentCard>
-                <StudentCard image={Etienne} bio="test" name="Étienne Lescarbeault" title="Électronique"></StudentCard>
-                <StudentCard image={Zachary} bio="test" name="Zachary Ouellet" title="Structure"></StudentCard>
+                <StudentCard image={Francis} bio="test" 
+                name="Francis Bouchaud" title="Hydrogène"></StudentCard>
+                <StudentCard image={Etienne} bio="Je m'occupe des systèmes électroniques donnant vie au bateau, me permettant ainsi de prouver la viabilité de l'hydrogène vert dans le milieu maritime."
+                 name="Étienne Lescarbeault" title="Électronique"></StudentCard>
+                <StudentCard image={Zachary} bio="test" 
+                name="Zachary Ouellet" title="Structure"></StudentCard>
                 </div>
                 <div className="cards-container">
-                    <StudentCard image={Xavier} bio="test" name="Xavier Corbeil" title="Électrique"></StudentCard>
-                    <StudentCard image={Aurelie} bio="test" name="Aurélie Jund" title="Développement Durable"></StudentCard>
+                    <StudentCard image={Xavier} bio="test" 
+                    name="Xavier Corbeil" title="Électrique"></StudentCard>
+                    <StudentCard image={Aurelie} bio="Ce que j’aime chez Exocet c’est le défi d’utiliser l’hydrogène comme source d'énergie et de se donner les moyens d’arriver à un beau résultat"
+                     name="Aurélie Jund" title="Développement Durable"></StudentCard>
                 </div>
             </div>
             <div className="member-type-container">
                <p className="team-subtitle">Membres</p>
                 <div className="cards-container">
-                    <StudentCard image={Eliot} bio="test" name="Eliot Fondère" title="Membre informatique"></StudentCard>
-                    <StudentCard image={Guillaume} bio="test" name="Guillaume Chénier" title="Membre informatique"></StudentCard>
-                    <StudentCard image={Dorian} bio="test" name="Dorian Emmanuel Bernard" title="Membre structure"></StudentCard>
-                    <StudentCard image={Mathilda} bio="test" name="Mathilda Herrell" title="Membre logiciel"></StudentCard>
+                    <StudentCard image={Eliot} bio="J'ai été en charge de la programmation du bateau et j'ai collaboré à la création des circuits imprimés et au design du système électronique" 
+                    name="Eliot Fondère" title="Membre informatique"></StudentCard>
+                    <StudentCard image={Guillaume} bio="test" 
+                    name="Guillaume Chénier" title="Membre informatique"></StudentCard>
+                    <StudentCard image={Dorian} bio="test" 
+                    name="Dorian Emmanuel Bernard" title="Membre structure"></StudentCard>
+                    <StudentCard image={Mathilda} bio="test" 
+                    name="Mathilda Herrell" title="Membre logiciel"></StudentCard>
                     <StudentCard image={Charles} 
                     bio="
                     eeeeeeeeeeeeeee eeeeeeeeeeeeeee eeeeeeeeeeeeeee eeeeeeeeeeeeeee eeeeeeeeeeeeeee eeeeeeeeeeeeeee eeeeeeeeeeeeeee eeeeeeeeeeeeeee eeeeeeeeeeeeeee eeeeeeeeeeeeeee 
